cd4be417e8e62c4368c2bbd663c666193637489c
Daniel Gómez H <danielivgho5@gmail.com>
2023-08-15 14:30:57 -0500

    Sets y Gets agregados a class.js

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -1,28 +1,50 @@
 class Persona {
+  //Declarar las propiedades aquí es opcional
   nombre = "";
   codigo = "";
   frase = "";
+  comida = "";
 
   constructor(
     nombre = "Sin nombre",
     codigo = "Sin código",
     frase = "Sin frase"
   ) {
-    if (!nombre) throw Error("Necesitamos el nombre");
-
     this.nombre = nombre;
     this.codigo = codigo;
     this.frase = frase;
+  }
+
+  set setComidaFavorita(comida) {
+    this.comida = comida.toUpperCase();
+  }
+
+  get getcomidaFavorita() {
+    return `La comida favorita de ${this.nombre} es ${this.comida}`;
+  }
+
+  quienSoy() {
+    console.log(`Soy ${this.nombre} y mi identidas es ${this.codigo}`);
+  }
+
+  miFrase() {
+    this.quienSoy();
+    console.log(`${this.codigo} dice "${this.frase}"`);
   }
 }
 
 const spiderman = new Persona(
   "Peter Parker",
-  "Spider",
+  "Spiderman",
   "Soy tu vecino spiderman"
 );
 
 const ironman = new Persona("Tony Stark", "Ironman", "Yo soy Ironman");
 
+spiderman.miFrase();
+
+spiderman.setComidaFavorita = "Pastel de Cereza";
+
+console.log(spiderman.getcomidaFavorita);
+
 console.log(spiderman);
-console.log(ironman);
